802bbc1079c6188ac5d22f290a1d0083d15b9541
Mani Soundararajan <root@msound.net>
2025-02-25 13:31:04 -0500

    chore: add version

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"log"
+	"log/slog"
 	"net/http"
 
 	"github.com/msound/hellopod/pkg/app"
 	"github.com/msound/hellopod/pkg/config"
 )
 
+const APP_VERSION = "v0.0.1"
+
 func main() {
+	slog.Info("Starting hellopod", "version", APP_VERSION)
 	appConfig := config.LoadConfig()
 	app := app.NewApp(appConfig)
 
